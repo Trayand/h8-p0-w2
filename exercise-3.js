ed65c1e366001319916f7cac93827dca4f34ba0d
Trayand <patradyn@gmail.com>
2019-09-24 21:53:49 +0700

    exercise-4

---
+++ exercise-3.js
@@ -0,0 +1,20 @@
+var nama = '';
+var peran = '';
+
+if (nama == ''){
+  console.log('"Nama harus diisi!"');
+} else if (peran == ''){
+  console.log('"Halo ' + nama + ', Pilih peranmu untuk memulai game!"');
+} else if (peran == "Ksatria"){
+  console.log('"Selamat datang di Dunia Proxytia, ' + nama + '."')
+  console.log('"Halo '+ peran + ' ' + nama + ', kamu dapat menyerang dengan senjatamu!"')
+} else if (peran == 'Tabib') {
+  console.log('"Selamat datang di Dunia Proxytia, ' + nama + '."')
+  console.log('"Halo '+ peran + ' ' + nama + ', kamu akan membantu temanmu yang terluka."');
+} else if (peran == 'Penyihir') {
+  console.log('"Selamat datang di Dunia Proxytia, ' + nama + '."')
+  console.log('"Halo '+ peran + ' ' + nama + ', ciptakan keajaiban yang membantu kemenanganmu!"') 
+} else {
+  console.log('"Halo ' + nama + ',');
+  console.log('Pilih peranmu di antara "Ksatria", "Tabib" atau "Penyihir".');
+}
